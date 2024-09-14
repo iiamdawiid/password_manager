718c53440f527a45513af85e102296f423a00a13
David Jachym <jachym19@gmail.com>
2024-09-13 20:36:46 -0500

    added delete_password; added styling to error codes and output

--- password_manager.py
+++ password_manager.py
@@ -30,12 +30,12 @@ def start_menu():
         try:
             user_choice = int(input("Enter choice: "))
             if user_choice not in {1, 2, 3}:
-                print("ERROR: Please enter a valid choice")
+                print(f"{Fore.RED}ERROR: Please enter a valid choice{Style.RESET_ALL}")
             else:
                 break
 
         except ValueError:
-            print("ERROR: Please enter a number")
+            print(f"{Fore.RED}ERROR: Please enter a number{Style.RESET_ALL}")
 
     if user_choice == 1:
         display_passwords()
@@ -48,26 +48,39 @@ def start_menu():
         delete_password()
 
 
-def display_passwords():
+def display_passwords(delete_password=False):
+    # print only if this function is not called from delete_password
+    if not delete_password:
+        print(
+            "\n"
+            + f"{Fore.YELLOW}{'DISPLAY PASSWORDS'.center(30, '=')}{Style.RESET_ALL}"
+        )
+
     # require MASTER PASSWORD for login to ensure security
-    print("\n" + "DISPLAY PASSWORDS".center(30, "="))
-    master_password = input("Enter master password: ")
-    encrypted_maspas = encrypt_password(master_password)
-    user = password_collection.find({"master_password": encrypted_maspas})
+    while True:
+        master_password = input("Enter master password: ")
+        encrypted_maspas = encrypt_password(master_password)
+        user = password_collection.find({"master_password": encrypted_maspas})
 
-    user_records = list(user)
+        user_records = list(user)
 
-    if user:
-        for record in user_records:
-            # IMPLEMENT TABULATE LIBRARY TO IMPROVE OUTPUT
-            master_password = record["master_password"]
-            website = record["website_name"]
-            password = decrypt_password(record["password"])
+        if user_records:
+            print(f"LOGIN: {Fore.GREEN}SUCCESS{Style.RESET_ALL}")
             print(
-                f"\nMaster password: {master_password}\nWebsite: {website}\nPassword: {password}"
+                "\n"
+                + f'{Fore.GREEN}{"SAVED PASSWORDS".center(30, "=")}{Style.RESET_ALL}'
             )
-    else:
-        print(f"No passwords stored for user")
+            for record in user_records:
+                # IMPLEMENT TABULATE LIBRARY TO IMPROVE OUTPUT
+                master_password = record["master_password"]
+                website = record["website_name"]
+                password = decrypt_password(record["password"])
+                print(f"{website}: {Fore.YELLOW}{password}{Style.RESET_ALL}\n")
+            break
+
+        else:
+            print(f"LOGIN: {Fore.RED}FAILED{Style.RESET_ALL}")
+            print(f"{Fore.RED}No passwords stored for user{Style.RESET_ALL}")
 
 
 def store_password():
@@ -77,19 +90,21 @@ def store_password():
     encrypted_master_password = encrypt_password(master_password)
     website_name = input("Enter website name (Ex. facebook): ")
     print("".center(30, "="))
-    print("1: Enter Personal Password\n2: Generate Secure Password")
+    print(
+        f"{Fore.YELLOW}1: Enter Personal Password{Style.RESET_ALL}\n{Fore.GREEN}2: Generate Secure Password{Style.RESET_ALL}"
+    )
     print("".center(30, "="))
 
     while True:
         try:
             password_choice = int(input("Enter choice:  "))
             if password_choice not in {1, 2}:
-                print("ERROR: Valid choices are 1 or 2")
+                print(f"{Fore.RED}ERROR: Valid choices are 1 or 2{Style.RESET_ALL}")
             else:
                 break
 
         except ValueError:
-            print("ERROR: Please enter a number")
+            print(f"{Fore.RED}ERROR: Please enter a number{Style.RESET_ALL}")
 
     if password_choice == 1:
         password = input("Enter password: ")
@@ -97,7 +112,7 @@ def store_password():
 
     else:
         password = generate_password()
-        print(f"Generated Password: {password}")
+        print(f"Generated Password: {Fore.GREEN}{password}{Style.RESET_ALL}")
 
     # after getting password encrypt it
     encrypted_password = encrypt_password(password)
@@ -110,10 +125,31 @@ def store_password():
         "password": encrypted_password,
     }
     password_collection.insert_one(website_data)
-    print("Password stored successfully")
+    print(f"PASSWORD STORED: {Fore.GREEN}SUCCESS{Style.RESET_ALL}")
 
 
-def delete_password(): ...
+def delete_password():
+    print("\n" + f"{Fore.RED}{'DELETE PASSWORD'.center(30, '=')}{Style.RESET_ALL}")
+    display_passwords(True)
+    # display selected entry user desires to delete
+    while True:
+        try:
+            website_name = input("Site associated with password: ")
+            display_choice = password_collection.find_one(
+                {"website_name": website_name}
+            )
+            if display_choice:
+                password_collection.delete_one({"website_name": website_name})
+                print(f"DELETE: {Fore.GREEN}SUCCESS{Style.RESET_ALL}")
+                break
+            else:
+                print(f"DELETE: {Fore.RED}FAILED{Style.RESET_ALL}")
+                print(
+                    f"Website '{website_name}' not found: Please double-check and try again."
+                )
+
+        except Exception as e:
+            print(f"{Fore.RED}Error:{Style.RESET_ALL} {e}")
 
 
 if __name__ == "__main__":
